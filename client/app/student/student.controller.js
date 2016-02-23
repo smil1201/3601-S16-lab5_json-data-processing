e768995a4b37acfacdc5381b35b61a6ffe7861b8
smil1201 <mill5978@morris.umn.edu>
2016-02-22 20:15:29 -0600

    SM: fixed client studentController functionality

--- client/app/student/student.controller.js
+++ client/app/student/student.controller.js
@@ -1,9 +1,6 @@
 'use strict';
 
 angular.module('3601S16Lab5JsonDataProcessingApp')
-  .controller('StudentCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  })
   .controller('StudentCtrl', function ($scope, Student) {
     $scope.students = Student.query();
   });
