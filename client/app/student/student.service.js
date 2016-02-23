982fb5b0e7213316de76a2c309e8d185c99bc738
smil1201 <mill5978@morris.umn.edu>
2016-02-22 20:20:01 -0600

    SM: remove placeholder text from student service

--- client/app/student/student.service.js
+++ client/app/student/student.service.js
@@ -1,21 +1,6 @@
 'use strict';
 
-angular.module('studentManagerApp')
+angular.module('3601S16Lab5JsonDataProcessingApp')
   .factory('Student', function($resource) {
     return $resource('/api/students/:id');
-  });
-
-angular.module('3601S16Lab5JsonDataProcessingApp')
-  .factory('student', function () {
-    // Service logic
-    // ...
-
-    var meaningOfLife = 42;
-
-    // Public API here
-    return {
-      someMethod: function () {
-        return meaningOfLife;
-      }
-    };
   });
