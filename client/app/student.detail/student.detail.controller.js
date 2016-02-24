3c7e4a300018dfa779711d5e091000757f640e01
smil1201 <mill5978@morris.umn.edu>
2016-02-24 14:20:02 -0600

    SM: update student detail html and functionality

--- client/app/student.detail/student.detail.controller.js
+++ client/app/student.detail/student.detail.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('3601S16Lab5JsonDataProcessingApp')
-  .controller('StudentDetailCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('StudentDetailCtrl', function ($scope, $stateParams, Student) {
+    $scope.student = Student.get({ id: $stateParams.id });
   });
